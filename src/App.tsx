6e13f439903a5ce1537813520055a5a9259b2f00
AdityaAA2004 <anandad1@msu.edu>
2025-05-27 20:18:33 -0400

    Initial commit with amplify and google configured

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,27 @@ import FeatureCard from './components/FeatureCard';
 import OnboardingSection from './components/OnboardingSection';
 import DocumentsSection from './components/DocSection';
 //import TextReveal from './components/TextReveal';
+import {Amplify} from 'aws-amplify';
+import awsExports from './aws-exports.js';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+
+Amplify.configure(awsExports);
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <Cal/>
-      <StudentsInfo/>
-      <OnboardingSection/>
-      <DocumentsSection/>
-      <FeatureCard/>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <div className="App">
+        <Navbar />
+        <Home />
+        <Cal/>
+        <StudentsInfo/>
+        <OnboardingSection/>
+        <DocumentsSection/>
+        <FeatureCard/>
 
-      <Footer/>
-    </div>
+        <Footer/>
+      </div>
+    </GoogleOAuthProvider>
   );
 };
 
