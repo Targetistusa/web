a7936d40e0d3ecdbe896e8e401f0b40244a2cb49
AdityaAA2004 <anandad1@msu.edu>
2025-07-21 00:46:45 -0400

    Start changes for website for B2B

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '../styles/Home.css';
 import Squares from '../Squares/Squares';
 import FadeContent from '../FadeContent/FadeContent';
 import ShinyText from '../ShinyText/ShinyText';
 import StarBorder from '../StarBorder/StarBorder';
-
+import VariableProximity from '../VariableProximity/VariableProximity';
+import DarkVeil from './DarkVeil/DarkVeil';
 const Home: React.FC = () => {
+  const containerRef = useRef(null); 
   return (
     <div className="container">
       {/* Squares as animated background */}
       <div className="squares-background">
-        <Squares
-          speed={0.5}
-          squareSize={60}
-          direction="down"
-          borderColor="rgba(0, 0, 0, 0.11)"
-          hoverFillColor="rgba(167, 87, 215, 0.4)"
+        <DarkVeil
+          speed={2.2}
+          hueShift={342}
+          noiseIntensity={0.11}
+          scanlineFrequency={5}
+          scanlineIntensity={1}
+          warpAmount={1}
         />
       </div>
 
       {/* Fade-in text and CTA on top */}
       <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
         <div className="content-wrapper">
-          <h1 className="title-1">Achieving Targets Has Never</h1>
-          <h1 className="title-2">Been This Easy</h1>
+          <div ref={containerRef} style={{position: 'relative'}}>
+            <VariableProximity
+              label={'Schedule at the speed of thought'}
+              className={'title-1'}
+              fromFontVariationSettings="'wght' 400, 'opsz' 30"
+              toFontVariationSettings="'wght' 700, 'opsz' 40"
+              containerRef={containerRef}
+              radius={100}
+              falloff="gaussian"
+            />
+          </div>
 
           <div className="button-container-main" onClick={() => window.open('https://apps.apple.com/us/app/targetist/id6523418234', '_blank')}>
-            <StarBorder as="button" className="button-1" color="purple" speed="2s">
-              <ShinyText className="button-txt" text="try it now!" disabled={false} speed={1} />
-            </StarBorder>
+            <button></button>
           </div>
         </div>
       </FadeContent>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/ResourcesModal.css';
 import '../styles/Navbar.css';
-import { FaBars, FaTimes } from "react-icons/fa";
 import { isMobile, isTablet, isIOS } from 'react-device-detect';
-import Swal from 'sweetalert2';
-import { Link } from "react-scroll";
+
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
   const [scrolled, setScrolled] = useState(false);
-
-  const toggleNav = () => setNav(!nav);
-  const openPrivacyPolicy = () => {
-    window.open(`/privacy-policy.html`, "_blank");
-  };
-
   useEffect(() => {
     const handleScroll = () => {
       const isScrolled = window.scrollY > 50;
@@ -29,7 +20,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+    <nav className={`navbar scrolled`}>
       <div className="navbar-container">
         <div className="navbar-flex">
           {/* Logo and Title */}
@@ -38,74 +29,11 @@ const Navbar = () => {
             <span className="navbar-title">Targetist</span>
           </div>
 
-          {/* Hamburger Menu */}
-          {/* <button onClick={toggleNav} className="hamburger-menu-style" aria-label="Toggle menu">
-            {!nav ? <FaBars size={30} /> : <FaTimes size={30} />}
-          </button> */}
-
-
-          {nav && (
-            <ul className="mobile-menu">
-              <div onClick={toggleNav} style={{ position: "absolute", top: 10, left: 10 }}>
-                <FaTimes size={30} style={{ color: "#770fa7" }} />
-              </div>
-              <Link smooth={true} duration={500} onClick={() => setNav(!nav)} to="features" className="nav-item">Features</Link>
-              <a href="#" className="nav-item" onClick={() => Swal.fire({
-                title: "Not found",
-                text: "Sorry, this page is still under development. We will be back soon!",
-                icon: "warning",
-                iconColor: 'red',
-                showConfirmButton: false,
-                showCancelButton: true,
-                cancelButtonColor: "#d33",
-                cancelButtonText: "Close",
-              })}>Resources</a>
-              <Link smooth={true} duration={500} onClick={() => setNav(!nav)} to="pricing" className="nav-item">Pricing</Link>
-
-              <a href="#" className="nav-item" onClick={() => Swal.fire({
-                title: "Not found",
-                text: "Sorry, this page is still under development. We will be back soon!",
-                icon: "warning",
-                iconColor: 'red',
-                showConfirmButton: false,
-                showCancelButton: true,
-                cancelButtonColor: "#d33",
-                cancelButtonText: "Close",
-              })}>Students</a>
-            </ul>
-          )}
-
-          {/* Navbar Items */}
-          <div className="navbar-items">
-            <Link smooth={true} duration={500} onClick={() => setNav(!nav)} to="features" className="nav-item">Features</Link>
-            <a href="#" className="nav-item" onClick={() => Swal.fire({
-              title: "Not found",
-              text: "Sorry, this page is still under development. We will be back soon!",
-              icon: "warning",
-              iconColor: 'red',
-              showConfirmButton: false,
-              showCancelButton: true,
-              cancelButtonColor: "#d33",
-              cancelButtonText: "Close",
-            })}>Resources</a>
-            <a href="#" className="nav-item" onClick={() => Swal.fire({
-              title: "Not found",
-              text: "Sorry, this page is still under development. We will be back soon!",
-              icon: "warning",
-              iconColor: 'red',
-              showConfirmButton: false,
-              showCancelButton: true,
-              cancelButtonColor: "#d33",
-              cancelButtonText: "Close",
-            })}>Students</a>
-            <a href="#" onClick={openPrivacyPolicy} className="nav-item">Privacy Policy</a>
-          </div>
-
           {/* CTA Button */}
           {!isIOS && !isMobile && !isTablet && (
             <div className="navbar-cta">
               <button className="button-download"
-              onClick={() => window.open('https://apps.apple.com/us/app/targetist/id6523418234', '_blank')}>Try Now</button>
+              onClick={() => window.open('https://apps.apple.com/us/app/targetist/id6523418234', '_blank')}>Resources</button>
             </div>
           )}
         </div>
