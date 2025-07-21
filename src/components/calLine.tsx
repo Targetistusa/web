ccc3bbda199edcd8505f870c49367240d5c6785b
banerj37 <banerj37@msu.edu>
2025-07-21 07:41:50 -0400

    need more work on the latter sections

--- src/components/calLine.tsx
+++ src/components/calLine.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../styles/calLine.css';
+import VariableProximity from '../VariableProximity/VariableProximity';
+import CardSwap, { Card } from '../CardSwap/CardSwap';
 
 const Cal: React.FC = () => {
   return (
     <div className="new-section-container">
       {/* Section Header */}
       <div className="section-header">
-        <h2 className="heading1">Let Time Work for You</h2>
+        <VariableProximity
+              label={'The OS for your Human Time'}
+              className={'header-text'}
+              fromFontVariationSettings="'wght' 400, 'opsz' 10"
+              toFontVariationSettings="'wght' 700, 'opsz' 40"
+              radius={100}
+              falloff="gaussian"
+            />
       </div>
 
       {/* Gray Card Container */}
@@ -24,8 +33,29 @@ const Cal: React.FC = () => {
 
         {/* Right Section with Images */}
         <div className="card-images">
-          <img src="/img2.png" alt="Phone 1" className="phone-image1" />
-          <img src="/img1.png" alt="Phone 2" className="phone-image2" />
+          {/* <img src="/img2.png" alt="Phone 1" className="phone-image1" />
+          <img src="/img1.png" alt="Phone 2" className="phone-image2" /> */}
+          <div style={{ height: '600px', position: 'relative' }}>
+          <CardSwap
+            cardDistance={60}
+            verticalDistance={70}
+            delay={5000}
+            pauseOnHover={false}
+            >
+            <Card>
+              <h3>Card 1</h3>
+              <p>Your content here</p>
+            </Card>
+            <Card>
+              <h3>Card 2</h3>
+              <p>Your content here</p>
+            </Card>
+           <Card>
+              <h3>Card 3</h3>
+              <p>Your content here</p>
+            </Card>
+          </CardSwap>
+        </div>
         </div>
       </div>
     </div>
